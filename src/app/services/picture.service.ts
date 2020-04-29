8fcf6260ca30369ec87aea756e538806f4fabe70
Oscar Home <Oscar>
2020-04-29 00:58:16 -0400

    UI fixes

--- src/app/services/picture.service.ts
+++ src/app/services/picture.service.ts
@@ -25,7 +25,10 @@ export class PictureService {
         _id,
         title,
         imageUrl,
-        authorId
+        author {
+          name,
+          lastName
+         }
       }
     }`;
 
